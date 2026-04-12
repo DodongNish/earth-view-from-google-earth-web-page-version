d0816946639ed26d12901f4589e375c5aff3e66a
Dodong Nish <nisidon777@gmail.com>
2026-04-12 20:33:36 +0900

    Configure deployments

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: './',
+    base: '/earth-view-from-google-earth-web-page-version/',
+    build: {
+        outDir: 'dist',
+    }
 });
